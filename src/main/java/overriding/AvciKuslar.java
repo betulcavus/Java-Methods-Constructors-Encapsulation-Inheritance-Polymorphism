b1b3352cfe83c33fd0d1d5b8f2203efb8fc6126d
Betul Cavus <btlcvss@gmail.com>
2022-11-05 16:25:41 +0300

    inheritance

---
+++ src/main/java/overriding/AvciKuslar.java
@@ -0,0 +1,14 @@
+package overriding;
+
+public class AvciKuslar extends Kuslar{
+
+    protected boolean pencesiVarmi=true;
+    protected void hareket(){
+        System.out.println("Avci kuslar ucarlar");
+    }
+    protected void beslenme(){
+        System.out.println("Avci kuslar etle beslenir");
+    }
+}
+
+
---
+++ src/main/java/polymorphism/Toyota.java
@@ -0,0 +1,13 @@
+package polymorphism;
+
+public class Toyota {
+
+
+    public void motor(){
+
+        System.out.println("Toyota'da toyota marka motor kullanilir");
+    }
+    public void yakit(){
+        System.out.println("Toyota'da motora uygun yakit kullanilir");
+    }
+}
